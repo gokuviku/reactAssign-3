79ec3751ef4c6f90992a87ca9a9390c6bb31fb58
Vishal-8155 <69633165+gokuviku@users.noreply.github.com>
2024-06-11 12:22:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+import Content from './components/Content';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Sidebar from './components/SideBar';
+import "bootstrap/dist/css/bootstrap.min.css"
+
+
+
+
+function App() {
+  return (
+    <>
+
+      <Header />
+      <Sidebar />
+      <Content />
+      <Footer />
+
+    </>
+  )
+}
+
+export default App
+
+
+
+
+
